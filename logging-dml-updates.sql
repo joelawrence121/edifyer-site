1131cb5d50b8abc816c73608c5cf6c605d44c000
Joseph Lawrence <joelawrence121@hotmail.co.uk>
2020-08-10 14:25:50 +0100

    logging script fix

--- logging-dml-updates.sql
+++ logging-dml-updates.sql
@@ -1,14 +1,17 @@
 ALTER TABLE `edifyer_db`.`company`
-ADD COLUMN `count` INT NULL DEFAULT 0 AFTER `category`;
+DROP count;
 
-CREATE TABLE `edifyer_db`.`ratingLog` (
-  `rating_id` INT NOT NULL,
+DROP TABLE ratingLog;
+
+CREATE TABLE `edifyer_db`.`companyLog` (
+  `company_id` INT NOT NULL,
   `date` DATETIME NOT NULL,
-  PRIMARY KEY (`rating_id`, `date`),
-  CONSTRAINT `fk_ratingLog_id`
-    FOREIGN KEY (`rating_id`)
-    REFERENCES `edifyer_db`.`rating` (`rating_id`)
+  PRIMARY KEY (`company_id`, `date`),
+  CONSTRAINT `fk_companyLog`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `edifyer_db`.`company` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
 
 CREATE INDEX idx_company_name ON company (name);
